471f0c8344f491af9858c714314be839a8d2745a
Uday Krishnan <udaykrishnanvg@gmail.com>
2023-04-03 00:44:02 +0530

    Base commit, swiping and vibration gestures

---
+++ android/app/src/main/kotlin/com/example/asvi/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.asvi
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class MyPageView extends StatefulWidget {
+  const MyPageView({Key? key}) : super(key: key);
+
+  @override
+  _MyPageViewState createState() => _MyPageViewState();
+}
+
+class _MyPageViewState extends State<MyPageView> {
+  final PageController _controller = PageController();
+  int _currentPageIndex = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller.addListener(_onPageViewScrolled);
+  }
+
+  @override
+  void dispose() {
+    _controller.removeListener(_onPageViewScrolled);
+    _controller.dispose();
+    super.dispose();
+  }
+
+  void _onPageViewScrolled() {
+    final newPageIndex = _controller.page?.round() ?? 0;
+    if (newPageIndex != _currentPageIndex) {
+      setState(() {
+        _currentPageIndex = newPageIndex;
+      });
+      HapticFeedback.vibrate();
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return PageView(
+      controller: _controller,
+      children: const <Widget>[
+        Center(
+          child: Text('First Page'),
+        ),
+        Center(
+          child: Text('Second Page'),
+        ),
+        Center(
+          child: Text('Third Page'),
+        ),
+      ],
+    );
+  }
+}
